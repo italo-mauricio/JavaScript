3bf6cec5099eaf5bc23208649ce4817d0175569b
italo <italogithub@gmail.com>
2022-12-23 18:25:38 -0300

    utilizando variável em formato json

---
+++ decalaraJson.js
@@ -0,0 +1,20 @@
+var carro = { tipo: "Carro de passeio",
+        marca: "Ford",
+        placa: "XPTO 9998",
+        cor: "vermelho",
+        ano: "1998"
+}
+
+console.log(carro.tipo
+    
+    + "\r\n"
+    + carro.marca
+    + "\r\n"
+    + carro.placa
+    + "\r\n"
+    + carro.cor
+    + "\r\n"
+    + carro.ano
+    +"\r\n"
+    
+    )
