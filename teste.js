f77cbd0b5d631613013d190555b5d9f3429a3512
italo <italogithub@gmail.com>
2022-12-23 15:42:18 -0300

    treinando conversão de tipos

---
+++ teste.js
@@ -0,0 +1,14 @@
+var x = 10.5;
+var y = String(x);
+
+var b = true;
+var c = String(b);
+
+console.log(y);
+console.log(b);
+
+
+console.log(typeof(x));
+console.log(typeof(y));
+console.log(typeof(c));
+// desta forma vemos que eu posso declarar uma variável e logo após converter o seu tipo
