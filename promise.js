e46fae2c9948af088cfe4a96583f0728a8465c5a
italo <italomauricio98@gmail.com>
2023-10-12 13:27:27 -0300

    update

--- promise.js
+++ promise.js
@@ -12,15 +12,27 @@
 //    })
 
 
-fetch('https://api.github.com/users/italo-mauricio')
-    .then(response => {
-        response.json().then(body => {
-            console.log(body);
-        })
-    } )
-    .catch(err => {
+// fetch('https://api.github.com/users/italo-mauricio')
+//     .then(response => {
+//         response.json().then(body => {
+//             console.log(body);
+//         })
+//     } )
+//     .catch(err => {
+//         console.log(err);
+//     })
+//     .finally(() => {
+//         console.log("ok");
+//     })
+
+async function buscaDadosGithub(){
+    try {
+        const response = await fetch('https://api.github.com/users/italo-mauricio')
+        const body = await response.json();
+        console.log(body);
+    }
+    catch (err) {
         console.log(err);
-    })
-    .finally(() => {
-        console.log("ok");
-    })
+    }
+}
+buscaDadosGithub();
